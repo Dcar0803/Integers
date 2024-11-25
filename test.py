3ba2a30f7db2ff2e17efcbc41602dafae2065ba3
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-25 15:54:49 -0500

    Test: Created a test for the return of a square root of a number and raise an Exception

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 import random
+
+def test_sqrt_with_check():
+    assert sqrt_with_check(4) == 2.0, "Square root of 4 should be 2.0"
+    assert sqrt_with_check(9) == 3.0, "Square root of 9 should be 3.0"
+    try:
+        sqrt_with_check(-1)
+    except ValueError:
+        pass  # Expected exception
+    else:
+        assert False, "Expected ValueError for negative input"
