0da83499f74eef97270474ab359ae9d9f40f0ff5
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-25 16:08:20 -0500

    Doc: Created documentation for the square root function

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import math 
 
-def sqrt_with_check(number):
+def square_root(number):
+
+    """Returns the square root of a number and checks if the number is negative
+
+    Raises:
+        ValueError: If the number is negative
+
+    Returns:
+        _float: the square root value of a number
+    """
 
     if number < 0: 
         raise ValueError("Cannot calculate the square root of a negative number")
