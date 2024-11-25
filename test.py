1364e959828dda0e455f399b0bd21de1eb5f82ec
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-25 16:11:31 -0500

    Updated: changed the function name from sqrt_with_check to square_root function

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import random
 from main import square_root
 
 def test_sqrt_with_check():
-    assert sqrt_with_check(4) == 2.0, "Square root of 4 should be 2.0"
-    assert sqrt_with_check(9) == 3.0, "Square root of 9 should be 3.0"
+    assert square_root(4) == 2.0, "Square root of 4 should be 2.0"
+    assert square_root(9) == 3.0, "Square root of 9 should be 3.0"
     try:
-        sqrt_with_check(-1)
+        square_root(-1)
     except ValueError:
         pass  # Expected exception
     else:
